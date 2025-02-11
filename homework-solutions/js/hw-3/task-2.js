140da032baa10f52af39d214701a9bc13bb1c332
ida-que <i.am.anna.nikulina@gmail.com>
2025-02-11 23:46:18 +0100

    JS homework 3 is done

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,13 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+let item_num = 3;
+let item_str = String(item_num);
+let concatItemsSum = item_num + +(item_num + item_str) + +(item_num + item_str + item_str);
+console.log(concatItemsSum);
+
+
+// решение номер 2
+let num = 3;
+console.log(num + +(num + String(num)) + +(num + String(num) + String(num)));
