633118762de184d56a994b9056525949110a22ec
ida-que <i.am.anna.nikulina@gmail.com>
2025-02-23 22:31:06 +0100

    HW 6 tasks 2 is done (?), task 3 is done

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,28 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
-let resultNull;
+let resultUnique = []; 
+let resultNull = null; // небольшой костыль
+
+// ====== "Если все ваши пиццы есть у конкурента результатом будет "null" присвойте в переменную "resultNull"." ============
+// 
+// Условие выше трактовала немного иначе. Ниже объединила 2 массива и только потом сравнила. 
+// В данном случае resultNull будет равен null только если у конкурента есть все мои пиццы из ОБОИХ наборов.
+
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'].map(competitorPizza => competitorPizza.toLowerCase());
+
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+const allMyPizzas = [...myPizzasT1, ...myPizzasT2].map(myPizza => myPizza.toLowerCase());
+
+for (let myPizza of allMyPizzas) {
+  if (!competitorPizzas.includes(myPizza)) {
+    resultUnique.push(myPizza.replace(/^\w/, c => c.toUpperCase()));
+  }
+}
+if (!resultUnique.length) {
+  resultNull = null;
+}
 
 export { resultNull, resultUnique };
