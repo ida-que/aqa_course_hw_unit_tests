2bc825e30f5226479b785f195f0ba94fd4a825be
ida-que <i.am.anna.nikulina@gmail.com>
2025-02-24 22:36:42 +0100

    simplified HW6 task 2, added solution for each array

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,28 +13,54 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique = []; 
-let resultNull = null; // небольшой костыль
+// Для обоих массивов сразу, с объединением
 
-// ====== "Если все ваши пиццы есть у конкурента результатом будет "null" присвойте в переменную "resultNull"." ============
-// 
-// Условие выше трактовала немного иначе. Ниже объединила 2 массива и только потом сравнила. 
-// В данном случае resultNull будет равен null только если у конкурента есть все мои пиццы из ОБОИХ наборов.
+let resultUnique = []; 
+let resultNull;
 
 const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'].map(competitorPizza => competitorPizza.toLowerCase());
 
 const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
 const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 
-const allMyPizzas = [...myPizzasT1, ...myPizzasT2].map(myPizza => myPizza.toLowerCase());
+const allMyPizzas = [...myPizzasT1, ...myPizzasT2];
 
 for (let myPizza of allMyPizzas) {
-  if (!competitorPizzas.includes(myPizza)) {
-    resultUnique.push(myPizza.replace(/^\w/, c => c.toUpperCase()));
+  if (!competitorPizzas.includes(myPizza.toLowerCase)) {
+    resultUnique.push(myPizza);
   }
 }
 if (!resultUnique.length) {
   resultNull = null;
 }
+
+// Решение номер 2 - без объединения, для каждого из данных массивов
+
+// const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+// const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian']; // Для теста 1 - проверка resultUnique
+// const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']; // Для теста 2 - проверка resultNull
+
+// const competitorPizzasToLower = competitorPizzas.map(el => el.toLowerCase());
+
+// let resultUnique = []; 
+// let resultNull;
+
+// for (let myPizza of myPizzasT1) {
+//   if (!competitorPizzasToLower.includes(myPizza.toLowerCase())) {
+//     resultUnique.push(myPizza);
+//   }
+//   if (!resultUnique.length) {
+//     resultNull = null;
+//   }
+//   }
+
+// for (let myPizza of myPizzasT2) {
+//   if (!competitorPizzasToLower.includes(myPizza.toLowerCase())) {
+//     resultUnique.push(myPizza);
+//   }
+//   if (!resultUnique.length) {
+//     resultNull = null;
+//   }
+// }
 
 export { resultNull, resultUnique };
