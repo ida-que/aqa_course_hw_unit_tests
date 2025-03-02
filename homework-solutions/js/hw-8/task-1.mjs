ca76d6428ededdfdd3fef0cdd29b147421c126e9
ida-que <i.am.anna.nikulina@gmail.com>
2025-03-02 15:18:01 +0100

    hw 8 task 1 is done

--- homework-solutions/js/hw-8/task-1.mjs
+++ homework-solutions/js/hw-8/task-1.mjs
@@ -13,14 +13,25 @@
   8. every - проверьте, что все элементы массива двухзначные //false
 */
 const numbers = [7, 8, 2, 30, 85, 95, 77, 94, 37, 31];
+// Solution number one - using forEach. But the disadvantage is this method is that it doesn't create a new array, so first need to initialize the empty array.
+// let forEach = [];
+// numbers.forEach(number => number % 3 === 0 ? forEach.push(number) : null);
 
-let forEach;
-let map;
-let filter;
-let find;
-let sort;
-let reduce;
-let some;
-let every;
+// Solution number two - using filter (which is more optimal).
+let forEach = numbers.filter(number => number % 3 === 0);
+
+let map = numbers.map(number => number - numbers.length);
+
+let filter = numbers.filter((number, i, arr) => i > 0 && number > arr[i-1]);
+
+let find = numbers.find ((number, i) => number === i);
+
+let sort = [...numbers].sort((a, b) => a - b);
+
+let reduce = numbers.reduce((acc, number) => acc + number, 0);
+
+let some = numbers.some(number => number > 90);
+
+let every= numbers.every(number => number >= 10 && number < 100);
 
 export { forEach, map, filter, find, sort, reduce, some, every };
