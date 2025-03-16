167c8e4a783a41a509cd88b7b4ea2bc95ed607e0
ida-que <i.am.anna.nikulina@gmail.com>
2025-03-16 22:21:14 +0100

    task 1 done

--- homework-solutions/js/hw-10/task-1.mjs
+++ homework-solutions/js/hw-10/task-1.mjs
@@ -5,7 +5,15 @@
     Значения в строке должны ссылаться на контекст ЭТОГО ОБЪЕКТА, без подмен.
 */
 
-let qa;
+let qa = {
+  name: "Anna",
+  age: 27,
+  salary: 5000,
+  getInfo(greetingsWord) {
+    return `${greetingsWord}, my name is ${this.name}, I'm ${this.age} and my salary is ${this.salary}`;
+  },
+};
+console.log(qa.getInfo("Hello"));
 
 /*
  2. Changing the context
@@ -15,16 +23,22 @@ let qa;
   - Вызовите метод getInfo объекта qa c контекстом вызова объекта anotherQa с помощью метода apply()
  */
 
-let anotherQa;
+let anotherQa = {
+  name: "Jane",
+  age: 25,
+  salary: 6000,
+};
+
 
 // Используйте bind с greetingWord "Hello"
-let bindResult;
+let bindResult = qa.getInfo.bind(anotherQa)('Hello');
+console.log(bindResult);
 
 // Используйте call с greetingWord "Hi"
-let callResult;
+let callResult = qa.getInfo.call(anotherQa, "Hi");
 
 // Используйте apply с greetingWord "Hey"
-let applyResult;
+let applyResult = qa.getInfo.apply(anotherQa, ["Hey"]);
 
 /*
  3. Closures
@@ -38,7 +52,12 @@ let applyResult;
 */
 
 function createCounter() {
-  // Ваш код
+  let count = 0;
+  return () => {
+    count++;
+    console.log(`Function was called ${count} times`);
+    return count;
+  };
 }
 
 const functionCallCounter = createCounter();
